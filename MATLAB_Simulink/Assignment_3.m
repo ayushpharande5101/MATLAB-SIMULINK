d47c5740ee85589f2e3bfa19e82758dd87f49260
Ayush <ayushpharande5101@gmail.com>
2023-12-29 11:13:58 +0530

    Add files via upload

---
+++ MATLAB_Simulink/Assignment_3.m
@@ -0,0 +1,10 @@
+clc
+clear all
+close all
+format compact 
+
+x = 2;
+u = 1;
+
+sim("Maths_Eqn_2")
+
---
+++ MATLAB_Simulink/Project_2.m
@@ -0,0 +1,9 @@
+clc
+clear all
+close all
+format compact
+F = 2;
+A = 3;
+Ti = 0.5;
+
+sim('Maths_Eqn')
